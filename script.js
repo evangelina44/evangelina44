c3a3884a83e06a9c341794ab96a01f9e1a02684c
evangelina44 <evangelina.rodriguez@anima.edu.uy>
2026-04-29 18:30:23 -0300

    funcion

--- script.js
+++ script.js
@@ -55,3 +55,56 @@ function areaCirculo(radio) {
 
 console.log(areaCirculo(5));
 console.log(areaCirculo(10));
+
+function generarContrasena(longitud) {
+  const caracteres = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+
+  let contrasena = "";
+
+  for (let i = 0; i < longitud; i++) {
+    let indice = Math.floor(Math.random() * caracteres.length);
+    contrasena = contrasena + caracteres[indice];
+  }
+
+ {
+  return contraseña;
+}
+
+
